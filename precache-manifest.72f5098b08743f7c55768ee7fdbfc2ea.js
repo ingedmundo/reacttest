e662b4ecc3e8b7ede25773b1f2f6969eaf05f7cd
Edmundo Perez <edmundo@michelada.io>
2019-09-16 21:45:47 -0500

    Updates

--- precache-manifest.72f5098b08743f7c55768ee7fdbfc2ea.js
+++ precache-manifest.72f5098b08743f7c55768ee7fdbfc2ea.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "76425c61d813e598cad84e8b3e1e94eb",
+    "revision": "f228444f7f2d011fbbb370a325aecc9e",
     "url": "/reacttest/index.html"
   },
   {
-    "revision": "205f4deb3f56cfaf1e59",
+    "revision": "e4103fd4cb0e014c81cc",
     "url": "/reacttest/static/css/main.34de6062.chunk.css"
   },
   {
-    "revision": "7e5b5b0be8aa7f950ea3",
-    "url": "/reacttest/static/js/2.b9c6f6e5.chunk.js"
+    "revision": "3a968c111f8dd8babb5d",
+    "url": "/reacttest/static/js/2.019a4151.chunk.js"
   },
   {
-    "revision": "205f4deb3f56cfaf1e59",
-    "url": "/reacttest/static/js/main.0d155ae7.chunk.js"
+    "revision": "e4103fd4cb0e014c81cc",
+    "url": "/reacttest/static/js/main.2c9d98fb.chunk.js"
   },
   {
     "revision": "f0ad4b820f3ba30d1946",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/reacttest/precache-manifest.419dcbd43e95d5e3c1668a0e269abda8.js"
+  "/reacttest/precache-manifest.72f5098b08743f7c55768ee7fdbfc2ea.js"
 );
 
 self.addEventListener('message', (event) => {
