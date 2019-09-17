9968b4f7e092be6a0a284ce68f5ec16820462136
Edmundo Perez <edmundo@michelada.io>
2019-09-16 21:37:19 -0500

    Updates

---
+++ precache-manifest.419dcbd43e95d5e3c1668a0e269abda8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76425c61d813e598cad84e8b3e1e94eb",
+    "url": "/reacttest/index.html"
+  },
+  {
+    "revision": "205f4deb3f56cfaf1e59",
+    "url": "/reacttest/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "7e5b5b0be8aa7f950ea3",
+    "url": "/reacttest/static/js/2.b9c6f6e5.chunk.js"
+  },
+  {
+    "revision": "205f4deb3f56cfaf1e59",
+    "url": "/reacttest/static/js/main.0d155ae7.chunk.js"
+  },
+  {
+    "revision": "f0ad4b820f3ba30d1946",
+    "url": "/reacttest/static/js/runtime~main.88ce729e.js"
+  }
+]);
